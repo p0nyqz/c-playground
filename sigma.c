e0dd27f5f6ed8c68e57a04e7d1d7468e8f905f87
p0nyqz <93746632+p0nyqz@users.noreply.github.com>
2024-02-23 12:49:27 +0100

    feat: sigma - positive numbers sum

---
+++ sigma.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+#include "cs50.h"
+
+int sigma(int);
+
+int main(void) 
+{
+  int n;
+  do 
+  {
+      // printf("Positive integer please: ");
+      n = get_int("Positive integer please: ");
+  }
+  while (n < 1);
+
+  int answer = sigma(n);
+  printf("%i\n", answer);
+}
+
+int sigma(int m)
+{
+  int sum = 0;
+  for (int i = 1; i <= m; i++)
+  {
+    sum += i;
+  }
+  return sum;
+}
